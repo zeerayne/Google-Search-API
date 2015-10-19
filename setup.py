dff4a35291cabe93a9206db98e7b7114982f14b2
Agustín Benassi <agusbenassi@gmail.com>
2015-10-19 11:06:16 -0300

    Update travis so it deploys automatically to pip, again.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ test_requirements = [
     "nose-cov"
 ]
 setup(name='Google-Search-API',
-      version='1.1.9',
+      version='1.1.10',
       url='https://github.com/abenassi/Google-Search-API',
       description='Search in google',
       author='Anthony Casagrande, Agustin Benassi',
