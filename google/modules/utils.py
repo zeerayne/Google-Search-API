564694c25684c0f236a8023e6281f52ca2250b63
hugsy <hugsy+github@blah.cat>
2015-12-17 11:45:21 +1100

    in _get_search_url(), we removed the call to normalize_query() for the `q` parameter. The normalization is already performed by urllib.urlencode (L36). This simple hack allows Google keywords (site:, filetype:, etc.) to work as expected.

--- google/modules/utils.py
+++ google/modules/utils.py
@@ -32,7 +32,7 @@ def _get_search_url(query, page=0, per_page=10, lang='en'):
     # note: num per page might not be supported by google anymore (because of
     # google instant)
 
-    params = {'nl': lang, 'q': normalize_query(query).encode('utf8'), 'start':page * per_page, 'num':per_page}
+    params = {'nl': lang, 'q': query.encode('utf8'), 'start':page * per_page, 'num':per_page}
     params = urlencode(params)
     url = u"http://www.google.com/search?" + params
     #return u"http://www.google.com/search?hl=%s&q=%s&start=%i&num=%i" % (lang, normalize_query(query), page * per_page, per_page)
